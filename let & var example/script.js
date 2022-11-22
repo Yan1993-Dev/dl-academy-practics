ac19bc0d6d71502f39c7edd253a17b23626a1408
Yan <66394095+Yan1993-Dev@users.noreply.github.com>
2022-11-22 18:30:48 +0400

    Add files via upload

---
+++ let & var example/script.js
@@ -0,0 +1,15 @@
+if (true) {
+    var foo = 'Hello!';
+    let a = 'itlogia';
+    const b = 'lets learn!';
+  
+    console.log(foo); // 'Hello!';
+    console.log(a); // 'itlogia';
+    console.log(b); // 'let's learn!';
+  
+  }
+  
+  
+  console.log(foo); // 'Hello!';
+  console.log(a); // ReferenceError: a is not defined
+  console.log(b); // ReferenceError: b is not defined
